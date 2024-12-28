3ac46303409d5996b576867d6e63dfff4f75b1f4
ahmettbalki <ahmettbalki@gmail.com>
2024-12-29 02:24:09 +0300

    added configurations

--- src/projects/ECommerce.Domain/Entities/AppOperationClaim.cs
+++ src/projects/ECommerce.Domain/Entities/AppOperationClaim.cs
@@ -1,7 +1,6 @@
 ﻿using Core.Security.Entities;
 namespace ECommerce.Domain.Entities;
-public sealed class AppUser : User
+public sealed class AppOperationClaim : OperationClaim
 {
     public ICollection<AppUserOperationClaim> UserOperationClaims { get; set; } = null!;
-    public ICollection<Order> Orders { get; set; }
 }
--- src/projects/ECommerce.Domain/Entities/OrderItem.cs
+++ src/projects/ECommerce.Domain/Entities/OrderItem.cs
@@ -3,7 +3,7 @@ namespace ECommerce.Domain.Entities;
 public sealed class OrderItem : Entity<Guid>
 {
     public Guid ProductId { get; set; }
-    public ICollection<Product> Products { get; set; }
+    public Product Product { get; set; }
     public int Count { get; set; }
     public Guid OrderId { get; set; }
     public Order Order { get; set; }
